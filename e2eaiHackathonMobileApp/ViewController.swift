ccb136a4a9a40b7b1789637bcba74abf4b78aebd
Yusuf Özgül <yozgul@icloud.com>
2019-12-19 14:57:42 +0300

    proje temeli eklendi

---
+++ e2eaiHackathonMobileApp/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  e2eaiHackathonMobileApp
+//
+//  Created by Yusuf Özgül on 19.12.2019.
+//  Copyright © 2019 Yusuf Özgül. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var text: UITextView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    @IBAction func getResultButton(_ sender: Any)
+    {
+        getResult(key: text.text) { (value) in
+            DispatchQueue.main.async {
+                self.text.text = value
+            }
+            
+        }
+    }
+
+    func getResult(key: String, completion: @escaping (String) -> Void)
+    {
+        let url = URL(string: "https://mobileapi.hepsiburada.com/api/CategorySearchV5?categoryId=")
+        let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
+            guard let data = data else { return }
+            completion(String(data: data, encoding: .utf8)!)
+        }
+        task.resume()
+    }
+}
+
