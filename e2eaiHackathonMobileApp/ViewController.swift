9967a382a4d45240550bf093dd63dc9006e8f134
Yusuf Özgül <yozgul@icloud.com>
2019-12-19 16:25:04 +0300

    .

--- e2eaiHackathonMobileApp/ViewController.swift
+++ e2eaiHackathonMobileApp/ViewController.swift
@@ -29,7 +29,19 @@ class ViewController: UIViewController {
 
     func getResult(key: String, completion: @escaping (String) -> Void)
     {
-        let url = URL(string: "http://http://40.121.157.250/\(key)")
+        var term = key
+        term = term.replacingOccurrences(of: " ", with: "%20")
+        term = term.replacingOccurrences(of: "ü", with: "%C3%BC")
+        term = term.replacingOccurrences(of: "ş", with: "%C5%9F")
+        term = term.replacingOccurrences(of: "ı", with: "%C4%B1")
+        term = term.replacingOccurrences(of: "Ö", with: "%C3%96")
+        term = term.replacingOccurrences(of: "Ç", with: "%C3%87")
+        term = term.replacingOccurrences(of: "ğ", with: "%C4%9F")
+        term = term.replacingOccurrences(of: "Ş", with: "%C5%9E")
+        term = term.replacingOccurrences(of: "İ", with: "%C4%B0")
+        term = term.replacingOccurrences(of: "ç", with: "%C3%A7")
+        
+        let url = URL(string: "http://40.121.157.250/\(term)")
         let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
             guard let data = data else { return }
             completion(String(data: data, encoding: .utf8)!)
